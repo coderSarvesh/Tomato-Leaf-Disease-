dffe16ac1b6a15096e1b582cc8c9ce2d202d2d44
rane_sarvesh <ranesarvesh97@gmail.com>
2022-09-26 20:39:37 +0530

    json's added

---
+++ diseases/disease.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '*.module.css'
+import Link from 'next/link'
+
+
+const Disease = () => {
+  return (
+    <div className='styles.container'></div>
+  )
+}
+
+export default Disease
