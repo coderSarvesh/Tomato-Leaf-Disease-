715ed1f881f271d926e81ad9fa2d40654cac58fb
rane_sarvesh <ranesarvesh97@gmail.com>
2022-10-07 21:36:41 +0530

    testing personal 3

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,27 @@
 export default async function handler(req, res) {
 
-        res.status(200).json({title:"MU ADMISSIONS STARTED",desc:"Lorem28fkejesbsjkblzshgjgjrfiewhfknneklnveboimwfeaufbebfeqmffiefmegbbgnrnngjbrgksgjrjg"});
-    
+    res.status(200).json([
+        {
+            title: "MU ADMISSIONS STARTED",
+            desc: "Lorem28fkejesbsjkblzshgjgjrfiewhfknneklnveboimwfeaufbebfeqmffiefmegbbgnrnngjbrgksgjrjg"
+        },
+        {
+            title: "MU ADMISSIONS STARTED",
+            desc: "Lorem28fkejesbsjkblzshgjgjrfiewhfknneklnveboimwfeaufbebfeqmffiefmegbbgnrnngjbrgksgjrjg"
+        },
+        {
+            title: "MU ADMISSIONS STARTED",
+            desc: "Lorem28fkejesbsjkblzshgjgjrfiewhfknneklnveboimwfeaufbebfeqmffiefmegbbgnrnngjbrgksgjrjg"
+        },
+        {
+            title: "MU ADMISSIONS STARTED",
+            desc: "Lorem28fkejesbsjkblzshgjgjrfiewhfknneklnveboimwfeaufbebfeqmffiefmegbbgnrnngjbrgksgjrjg"
+        },
+        {
+            title: "MU ADMISSIONS STARTED",
+            desc: "Lorem28fkejesbsjkblzshgjgjrfiewhfknneklnveboimwfeaufbebfeqmffiefmegbbgnrnngjbrgksgjrjg"
+        }
+        
+    ]);
+
 }
