2329e43cc780d4cf3d4dbcc66ec60dc2a263b696
rane_sarvesh <ranesarvesh97@gmail.com>
2022-10-07 21:33:58 +0530

    testing personal 2

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,5 @@
 export default async function handler(req, res) {
 
-    if(req.method == 'GET') {
         res.status(200).json({title:"MU ADMISSIONS STARTED",desc:"Lorem28fkejesbsjkblzshgjgjrfiewhfknneklnveboimwfeaufbebfeqmffiefmegbbgnrnngjbrgksgjrjg"});
-    }
+    
 }
