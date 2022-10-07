fbd4852f4917bbc1b2f0aecba5697860cd219a28
rane_sarvesh <ranesarvesh97@gmail.com>
2022-10-08 00:47:12 +0530

    testing personal 4

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,8 @@
 export default async function handler(req, res) {
 
-    res.status(200).json([
+    res.status(200).json(
+        
+        
         {
             title: "MU ADMISSIONS STARTED",
             desc: "Lorem28fkejesbsjkblzshgjgjrfiewhfknneklnveboimwfeaufbebfeqmffiefmegbbgnrnngjbrgksgjrjg"
@@ -21,7 +23,6 @@ export default async function handler(req, res) {
             title: "MU ADMISSIONS STARTED",
             desc: "Lorem28fkejesbsjkblzshgjgjrfiewhfknneklnveboimwfeaufbebfeqmffiefmegbbgnrnngjbrgksgjrjg"
         }
-        
-    ]);
+        );
 
 }
